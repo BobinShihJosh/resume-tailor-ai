68ecdc7759c407c1e486ad2f3f0164b5ebd5506b
BobinShihJosh <bos2@uw.edu>
2024-05-03 17:53:42 -0700

    change db

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -27,8 +27,8 @@ export async function getUserById(userId: string) {
     await connectToDatabase();
 
     const user = await User.findOne({ clerkId: userId });
-    // console.log("getUserById: ", userId )
-    // console.log(" actual user: ", user)
+    console.log("getUserById: ", userId )
+    console.log(" actual user: ", user)
     if (!user) throw new Error("User not found");
 
     return JSON.parse(JSON.stringify(user));
